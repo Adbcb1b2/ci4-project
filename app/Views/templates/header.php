6a51247096087dd25e57f20efcc4276779281c24
Kim Richards <richards.kimc@gmail.com>
2025-03-05 18:44:25 +0000

    Amended page header text and title

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Jobs Board</title>
 
     <!-- Link to Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -27,8 +27,7 @@
         }
 
         .banner-title:hover{
-            font-weight: bold;
-            color: white;
+            color: darkgrey;
         }
 
         .align-center {
