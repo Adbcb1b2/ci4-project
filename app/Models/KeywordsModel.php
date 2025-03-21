8b2d30e2e1c2399ea3d6a65cd480491ba0d2d20b
Kim Richards <richards.kimc@gmail.com>
2025-03-21 20:43:19 +0000

    Implemented getSuggestions method to get keyword suggestions based on a partial term

---
+++ app/Models/KeywordsModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class KeywordsModel extends Model
+{
+    protected $table = 'keywords';
+    protected $allowedFields = ['keyword'];
+
+    /**
+     * Get keyword suggestions based on a partial term
+     *
+     * @param string $term
+     * @return array
+     */
+    public function getSuggestions($term)
+    {
+        return $this->like('keyword', $term)
+                    ->findAll(); 
+    }
+}
